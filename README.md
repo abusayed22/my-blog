68ad788bc8cab64b88cc9f3112fd70c3432d83de
abusayed22 <abusayed.in22@gmail.com>
2023-01-25 18:43:09 +0600

    readme file initiate done

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+
+<ul>
+  <li><b>CSS:</b>
+    <li>Tailwindcss</li>
+    <li>Metrial UI</li>
+  </li>
+  <li><b>extranal</b>
+    <li>react-router-dom</li>
+    <li> </li>
+  </li>
+
+</ul> 
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 function App() {
-  
+
   return (
     <div>
       hello
@@ -13,13 +13,13 @@ export default App;
 
 
 {/* <ul>
-        <li><b>CSS:</b>
-          <li>Tailwindcss</li>
-          <li>Metrial UI</li>
-        </li>
-        <li><b>extranal</b>
-          <li>react-router-dom</li>
-          <li> </li>
-        </li>
+  <li><b>CSS:</b>
+    <li>Tailwindcss</li>
+    <li>Metrial UI</li>
+  </li>
+  <li><b>extranal</b>
+    <li>react-router-dom</li>
+    <li> </li>
+  </li>
 
-      </ul> */}
+</ul>  */}

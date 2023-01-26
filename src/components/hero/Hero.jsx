6b7253b06c9a74fd9c918fb95911eb351ee6dd04
abusayed22 <abusayed.in22@gmail.com>
2023-01-26 22:54:29 +0600

    today ok

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import baner from "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fblog&psig=AOvVaw39hpH0JYDAMmD_bNGJJSHT&ust=1674837059835000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCIif16XU5fwCFQAAAAAdAAAAABAE.jpg"
+function Hero() {
+  return (
+    <div>
+        <img src={baner} alt="" />
+    </div>
+  )
+}
+
+export default Hero

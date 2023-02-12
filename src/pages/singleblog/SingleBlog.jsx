936f5527a13eeaa75d39ba29a42c19b3fc8c734f
Abu Sayed <abusayed.in22@gmail.com>
2023-02-12 19:20:50 +0600

    some disgn in single cart

--- src/pages/singleblog/SingleBlog.jsx
+++ src/pages/singleblog/SingleBlog.jsx
@@ -4,6 +4,7 @@ import Cart from "../../components/home/Cart";
 import { useDispatch, useSelector } from "react-redux";
 import singleBlogFetch from "../../redux/allBlog/thunk/singleBlogFetch";
 import { useParams } from "react-router-dom";
+import HomeLoaderCard from "../../components/loader/HomeLoaderCard";
 
 
 function SingleBlog() {
@@ -27,24 +28,21 @@ function SingleBlog() {
   if (!isError && isLoading) {
     return (<>
       <div className="dark:bg-black">
-        <div>
-          <img src={thumbnail} alt={author} className="w-[100%] h-[60vh]" />
+        <div className="w-[80%] h-[60vh] mx-auto bg-[#383737] animate-pulse">
         </div>
         <br />
-        <div className="flex">
+        <div className=" flex ">
           <div className="flex flex-col justify-center items-center w-[20%] space-y-1">
-            <Avatar src="" alt="" />
-            <b>{author}</b>
-            <p>{'A GOOD TEACHER'}</p>
-            <p>{"Passionate Programmer"}</p>
+            <Avatar src="" alt=""  className=" animate-pulse "/>
+            <b className="w-28 h-4 bg-[#383737] animate-pulse"></b>
+            <p className="w-28 h-3 bg-[#383737] animate-pulse"></p>
+            <div className="w-28 h-2 bg-[#383737] animate-pulse"></div>
           </div>
-          <div className="w-[80%] p-5">
-            <p
-              left
-              className="indent-12 selection:text-green first-letter:text-red first-letter:text-2xl text-justify dark:text-green dark:selection:text-yellow "
-            >
-              {description}
-            </p>
+          <div className="w-[80%]">
+            
+              <div className="w-[92%] h-5 bg-[#383737] animate-pulse mb-2"></div>
+              <div className="w-[92%] h-5 bg-[#383737] animate-pulse mb-2"></div>
+              <div className="w-[92%] h-5 bg-[#383737] animate-pulse mb-2"></div>
           </div>
         </div>
         <br />
@@ -53,10 +51,9 @@ function SingleBlog() {
             Related Blogs
           </p>
           <div className="flex flex-wrap justify-center space-x-5">
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
+              <HomeLoaderCard />
+              <HomeLoaderCard />
+              <HomeLoaderCard />
           </div>
           <div className="flex justify-center mt-10">
             
@@ -68,20 +65,18 @@ function SingleBlog() {
     return (
       <div className="dark:bg-black">
         <div>
-          <img src={thumbnail} alt={author} className="w-[100%] h-[60vh]" />
+          <img src={thumbnail} alt={author} className="w-[75%] h-[60vh] mx-auto" />
         </div>
         <br />
-        <div className="flex">
-          <div className="flex flex-col justify-center items-center w-[20%] space-y-1">
+        <div className="flex flex-col justify-center items-center md:flex ">
+          <div className="flex flex-col justify-center items-center w-[18%] space-y-1">
             <Avatar src="" alt="" />
-            <b>{author}</b>
-            <p>{'A GOOD TEACHER'}</p>
-            <p>{"Passionate Programmer"}</p>
+            <b className="text-gray">{author}</b>
           </div>
-          <div className="w-[80%] p-5">
-            <p
+          <div className="w-[92%] p-5">
+            <p type='text'
               left
-              className="indent-12 selection:text-green first-letter:text-red first-letter:text-2xl text-justify dark:text-green dark:selection:text-yellow "
+              className="indent-12 selection:text-green shadow-deep first-letter:text-yellow shadow- first-letter:text-2xl text-justify dark:text-green dark:selection:text-yellow brightness-100 dark:bg-[#a51515a8] rounded-sm p-5"
             >
               {description}
             </p>
@@ -93,10 +88,10 @@ function SingleBlog() {
             Related Blogs
           </p>
           <div className="flex flex-wrap justify-center space-x-5">
-            <Cart />
-            <Cart />
-            <Cart />
-            <Cart />
+            <HomeLoaderCard />
+            <HomeLoaderCard />
+            <HomeLoaderCard />
+            <HomeLoaderCard />
           </div>
           <div className="flex justify-center mt-10">
             <Pagination count={10} color="primary" />

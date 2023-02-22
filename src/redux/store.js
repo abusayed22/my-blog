a72476ee73016f62990f07a728bbed12257ee3c0
Abu Sayed <abusayed.in22@gmail.com>
2023-02-22 17:15:40 +0600

    persistStore implement & export with store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,11 @@ import {createStore, applyMiddleware} from "redux"
 import rootReducer from "./rootReucer"
 import  ThunkMiddleware  from "redux-thunk"
 import { composeWithDevTools } from 'redux-devtools-extension'
+import persistStore from "redux-persist/es/persistStore";
 
 export const store = createStore(rootReducer,
     
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    composeWithDevTools(applyMiddleware(ThunkMiddleware)))
+    composeWithDevTools(applyMiddleware(ThunkMiddleware)));
+
+export const persistor = persistStore(store);

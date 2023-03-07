cea63ceb7dd3bd41e3ef88ba96c6a7234cd517ce
Abu Sayed <abusayed.in22@gmail.com>
2023-03-07 21:03:54 +0600

    formik & yup install for form handle

--- src/redux/allBlog/thunk/likeThunk/doLikeTHunk.js
+++ src/redux/allBlog/thunk/likeThunk/doLikeTHunk.js
@@ -1,18 +1,17 @@
 // like thunk here becouse updating thunk with update redux single state
-
 import axios from "axios";
-import { liked, liked_error, liked_loading, post_comment, } from "../../action";
+import { liked, liked_error, liked_loading } from "../../action";
 
 const doLikeThunk = (blogId,likedObject) => async(dispatch) => {
     dispatch(liked_loading());
-    const { id, title, description, author, date, duration, views, tags, link, thumbnail, like, comments } = likedObject;
+    const {id,name, title, description,  date, duration, views,Email, tags, link, thumbnail, like, comments} = likedObject;
     try {
-        
         const res = await axios.put(`http://localhost:9000/videos2/${blogId}`, {
             id, 
             title, 
             description, 
-            author, 
+            Email,
+            name, 
             date,
             duration, 
             views, 
@@ -23,12 +22,7 @@ const doLikeThunk = (blogId,likedObject) => async(dispatch) => {
             comments
         },
             { headers: { "Content-type": "application/json; charset=UTP-8" } });
-
-        // const updatedData = await res.data()
-        // console.log(updatedData);
         dispatch(liked(likedObject))
-
-
     } catch (error) {
         dispatch(liked_error(error));
     }

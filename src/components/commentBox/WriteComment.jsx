4e3abc5c95573102832e878eb55bd06cbb724dd5
Abu Sayed <abusayed.in22@gmail.com>
2023-03-21 17:00:28 +0600

    comment related bug fixed

--- src/components/commentBox/WriteComment.jsx
+++ src/components/commentBox/WriteComment.jsx
@@ -11,6 +11,8 @@ function WriteComment({ id: blogId }) {
   const { blog, isLoading, isError } = useSelector(
     (state) => state.allBlog.blog
   );
+  const { user } = useSelector(state => state.user);
+  const {email:userEmail} = user
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,7 +23,7 @@ function WriteComment({ id: blogId }) {
     author,
     date,
     duration,
-    Email,
+    email,
     views,
     tags,
     link,
@@ -32,7 +34,6 @@ function WriteComment({ id: blogId }) {
 
   const [isOpen, setIsOpen] = useState(false);
   const isChecked = useAuthChecked();
-
   // comment submit handler & more functionality for valid user
   const submitHandler = (e) => {
     e.preventDefault();
@@ -45,7 +46,7 @@ function WriteComment({ id: blogId }) {
         date,
         duration,
         views,
-        Email,
+        email,
         tags,
         link,
         thumbnail,
@@ -55,6 +56,7 @@ function WriteComment({ id: blogId }) {
           {
             id: uniqueId(),
             com: input,
+            whoCom: userEmail,
             date: moment(Date.now()).format("MMM Do YY"),
           },
         ],
